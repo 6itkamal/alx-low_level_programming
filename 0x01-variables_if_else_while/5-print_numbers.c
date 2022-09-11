a8bc8b1c1ba49b6255fc2d77fad2d64ec5934762
6itkamal <gitkamalabd@gmail.com>
2022-09-11 10:31:29 -0700

    New

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*srand(time(0));*/
+
+		/*n = rand() - RAND_MAX / 2;*/
+
+			/* your code goes there */
+		int i;
+
+		for (i = 0; i < 10; i++)
+		printf("%d", i);
+		printf("\n");
+
+			return (0);
+}

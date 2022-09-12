d6de7e90d108cd8813af72b078f72b29e5899b58
6itkamal <gitkamalabd@gmail.com>
2022-09-11 17:29:26 -0700

    News

---
+++ 0x01-variables_if_else_while/10-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*srand(time(0));*/
+
+		/*n = rand() - RAND_MAX / 2;*/
+
+			/* your code goes there */
+		int d;
+
+		for (d = '0'; d < 100; d++)
+		{
+			putchar((d / 10) + '0');
+			putchar((d % 10) + '0');
+
+			if (d != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+
+		return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*srand(time(0));*/
+
+		/*n = rand() - RAND_MAX / 2;*/
+
+			/* your code goes there */
+		int d;
+
+		for (d = '0'; d <= 10; d++)
+		{
+			putchar(d);
+			if
+				(d != '9');
+			{
+				putchar (',');
+					putchar (' ');
+			}
+		}
+		putchar('\n');
+		return (0);
+}

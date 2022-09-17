2e1474e364a053f8009f2279eabe8bf54a882883
6itkamal <gitkamalabd@gmail.com>
2022-09-16 17:04:04 -0700

    Nweeee

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- *
- * Return: 0
- *
- */
-
+* positive_or_negative(i): tests function that prints if
+* integer is positive or negative
+* main - func that prints interger
+* Return: 0
+*/
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-	return (0);
+return (0);
 }
